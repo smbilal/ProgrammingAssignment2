86269170f3076f6155e0b8d6388ea43b2f0952db
unknown <sm.billal@yahoo.co.uk>
2014-05-25 01:26:06 +0100

    Spell corrected

--- cachematrix.R
+++ cachematrix.R
@@ -1,6 +1,6 @@
 ## The functions are creating a matrix, calculating its inverse and then storing it into
 ## the cache for future retrievel requests. The second function first checks if the inverse already exists
-## in the cache and if it does, it skips the computation and simple pulls the output from cache provided
+## in the cache and if it does, it skips the computation and simply pulls the output from cache provided
 ## the contents of the matrix are the same
 
 
