977eab4ac9ccf5ead39e328f00ac9f96a5e8597f
unknown <sm.billal@yahoo.co.uk>
2014-05-25 01:59:08 +0100

    Some indenting work done on comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,12 +1,14 @@
+
 ## The functions are creating a matrix, calculating its inverse and then storing it into
 ## the cache for future retrievel requests. The second function first checks if the inverse already exists
 ## in the cache and if it does, it skips the computation and simply pulls the output from cache provided
 ## the contents of the matrix are the same
 
 
-## In "makeCacheMatrix" we are creating a matrix by either passing arguments along with the function call or by calling the "set"
-## method.It further saves the value of the inverse into a variable/cache using "setinverse" and "getinverse" is used to retrieve the value 
-## of inverse from the cache
+## In "makeCacheMatrix" we are creating a matrix by either passing arguments along with the function call or by calling the
+## "set" method.It further saves the value of the inverse into a variable/cache using "setinverse" and "getinverse" is used  
+## to retrieve the value of inverse from the cache
+
 
 makeCacheMatrix <- function(x = matrix()) {
   inv<-NULL
